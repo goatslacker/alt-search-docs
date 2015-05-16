28bc5037b0663a1d35d26a059e455056dc42aec7
Josh Perez <josh@goatslacker.com>
2015-05-16 00:34:31 -0700

    rm

--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -6,24 +6,12 @@ import SearchSource from '../sources/SearchSource'
 import SearchActions from '../actions/SearchActions'
 import marked from '../utils/marked'
 
-
 @decorate(alt)
 @datasource(SearchSource)
 class SearchStore {
   static displayName = 'SearchStore'
 
-  static config = {
-    // XXX
-    onSerialize(state) {
-      return state.index.toJSON()
-    }
-  }
-
   constructor() {
-//    console.log('@@@@@@@@@@@@', module.hot)
-    // XXX load in from localforage first...
-    // when teh async request comes back if its different than local storage
-    // then load it in otherwise return false.
     this.index = null
     this.documents = []
   }
@@ -39,27 +27,13 @@ class SearchStore {
       return obj
     }, {})
     this.index = lunr.Index.load(json.index)
-//    console.info('+>', this.index)
   }
 
   @bind(SearchActions.noIndexFound)
   failedLoadingIndex(err) {
-    console.info('Uh oh', err)
-    // throw an error?
+    this.index = null
+    console.error(err)
   }
-
-//  @bind(SearchActions.search)
-//  search(text = '') {
-//    this.results = text ? this.doSearch(text) : []
-//    this.searchTerm = text
-//  }
-
-//  doSearch(text) {
-    // Search and return the top 10 documents
-//    return this.index.search(text).map((result) => {
-//      return this.documents[result.ref]
-//    }).slice(0, 10)
-//  }
 }
 
 export default alt.createStore(SearchStore)
